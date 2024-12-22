e321d275ce5ee10e3c3de82ef0a6e409206f9353
Chandima Sandamali <chandimasandamali2002@gmail.com>
2024-12-22 22:23:09 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
 import Home from './pages/Home';
-import NavigationBar from './components/NavigationBar';
+import { BrowserRouter } from 'react-router-dom';
 
+function App(){
+return(
+  <></>
+);
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <div>
-        <NavigationBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />npm        
-          </Routes>
-      </div>
-    </BrowserRouter>
-  );
 }
 
 export default App;
