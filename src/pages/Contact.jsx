21dc271ec06acc27920b29a2484c7ad31299c7a0
Chandima Sandamali <chandimasandamali2002@gmail.com>
2025-01-15 22:13:38 +0530

    i made foundation

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Contact.css'
-import contact from '../assets/Contact/contact.jpg';
+
+
+
   export default function Contact() {
   return (
     <div class='contact'>
-      
-      <img src={contact} alt="contact" class="contact-pic" />
-      
-
-    </div>
+      <img src='src/assets/Contact/contact.jpg' class='contact_pic' />
+      <p class='contact_para'>GET IN TOUCH</p>
+      </div>
   )
 }
